ea8da31f36bcafe0562e2dd8fec6be54e25cf66e
amuseee <viksharrajesh@gmail.com>
2023-12-25 15:25:58 -0600

    bitflags dont workkfjfkgjhflghdfjkghfdkjghsd

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 // simple 6502 emulator written in rust
 // https://web.archive.org/web/20210429110213/http://www.obelisk.me.uk/6502/
 
+use std::fmt::Debug;
+use bitfield::bitfield;
+
 struct cpu {
-    pc: u8, // program counter
+    pc: u16, // program counter
     sp: u8, // stack pointer
-    a: u16, // registers
-    x: u16,
-    y: u16,
+    a: u8, // registers
+    x: u8,
+    y: u8,
+
+    // processor status flags - bitfields
+   bitfield!{
+        pub struct Statusflags(u8);
+
+   } 
+    /*
+        c++ implen of bitfield status flags
+        unsigned char C : 1;
+        unsigned char Z : 1;
+        unsigned char I : 1;
+        unsigned char D : 1;
+        unsigned char B : 1;
+        unsighed char V : 1;
+        unsigned char N : 1;
+    */
 }
 
 impl cpu {
