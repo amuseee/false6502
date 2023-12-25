9e8e494357f06986fb8cd8f499102303c304bb0f
amuseee <viksharrajesh@gmail.com>
2023-12-25 12:48:23 -0600

    first steps

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// simple 6502 emulator written in rust
+// https://web.archive.org/web/20210429110213/http://www.obelisk.me.uk/6502/
+
+struct cpu {
+    // program counter and stack pointer
+    let PC: u8;
+    let SP: u8;
+
+    let A, X, Y: u16; // registers
+}
+
+fn main() {
+
+}
