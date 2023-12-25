b4275ac59a23c0dc58be1d27eb3a73887147b17f
amuseee <viksharrajesh@gmail.com>
2023-12-25 13:38:12 -0600

    changed up structure to apply to rust

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@
 // https://web.archive.org/web/20210429110213/http://www.obelisk.me.uk/6502/
 
 struct cpu {
-    // program counter and stack pointer
-    let PC: u8;
-    let SP: u8;
+    pc: u8, // program counter
+    sp: u8, // stack pointer
+    a: u16, // registers
+    x: u16,
+    y: u16,
+}
 
-    let A, X, Y: u16; // registers
+impl cpu {
+    fn new() -> cpu {
+        cpu {
+            pc: 0,
+            sp: 0,
+            a: 0,
+            x: 0,
+            y: 0,
+        }
+    }
 }
 
 fn main() {
